81e16b1e3e46bcf74a4e87da344b34e9ae698df0
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2020-06-14 18:43:19 +0530

    moved the use-cases to a different directory

--- src/use-cases/list-teams.ts
+++
@@ -1,9 +0,0 @@
-export default function makeListTeams({ readDataFile }) {
-	return async function listTeams() {
-		let teamList = await readDataFile();
-
-		teamList = JSON.parse(teamList);
-
-		return teamList;
-	};
-}
